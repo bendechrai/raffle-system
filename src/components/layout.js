2cf23421c0b52ed4b2908c1c6122dadc677586b5
Ben Dechrai <ben@dechrai.com>
2020-11-17 12:10:48 +1100

    Update icon, add demo env to toml file, remove old images, create header bar, link to existing raffles,

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,19 +8,32 @@
 import React from "react"
 import PropTypes from "prop-types"
 
+import Image from '../components/image'
+
 import "./layout.css"
+import { Link } from "gatsby"
 
 const Layout = ({ children }) => {
   return (
     <>
       <div className="mainWrap">
+        <header>
+          <Link to="/">
+            <Image />
+          </Link>
+        </header>
         <main><div className="mainInner">
           {children}
         </div></main>
         <footer>
-        <p>Rafflist is a Lunar Productions creation <span role="img" aria-label="Heart">❤️</span> © {new Date().getFullYear()}, Lunar Productions Pty Ltd.</p>
-        <p>We take no fee for this service, and your payments go directly to Zonta Club of Melbourne's South East. Their credit card processing provider might deduct a fee from your payment.</p>
-        <p>For more information, or to start selling tickets yourself, <a href="https://bendechrai.com/contact">contact Ben Dechrai</a>.</p>
+          <p>Rafflist is a Lunar Productions creation <span role="img" aria-label="Heart">❤️</span> © {new Date().getFullYear()}, Lunar Productions Pty Ltd.</p>
+          <p>
+            We take no fee for this service, and your payments go directly to the raffle organiser.
+            Their credit card processing provider, Stripe, deducts a fee from your payment.
+            Rafflist stores no information about you.
+            The raffle organiser will draw raffle tickets based on the purchase data held by Stripe.
+          </p>
+          <p>For more information, or to start selling tickets yourself, <a href="https://bendechrai.com/contact">contact Ben Dechrai</a>.</p>
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import { Link } from "gatsby"
 import React from "react"
 
 import Layout from "../components/layout"
@@ -8,6 +9,19 @@ const IndexPage = () => (
     <SEO title="Welcome" />
     <h1>Welcome to Rafflist</h1>
     <p className="lead">Rafflist lets you sell raffle tickets online.</p>
+    <h2>Current Live Raffles</h2>
+    <div className="raffles">
+      <Link to="/zonta-mse-40">
+        <div className="raffle">
+          <h3>Zonta Club of Melbourne's South East 40th Birthday and 16 Day of Activism Raffle</h3>
+          <p>Raising funds for Zonta initiatives that support local women and their children impacted by family violence.</p>
+          <div class="metadata">
+            <p><strong>Starts:</strong> 25th Nov 2020</p>
+            <p><strong>Closes:</strong> 10th Dec 2020</p>
+          </div>
+        </div>
+      </Link>
+    </div>
     <p>For more information, or to start selling tickets yourself, <a href="https://bendechrai.com/contact">contact Ben Dechrai</a>.</p>
   </Layout>
 )
